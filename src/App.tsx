2bdc0ac78a5053284782ed23bead5383295b7b2e
hsujc <hsujc513@gmail.com>
2023-05-27 16:05:22 +0800

    add firestore.tsx to connect db, add useEffect() in App.tsx to test connection.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import {useEffect} from 'react';
+import firestore from './shared/firestore';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //test firestore
+  useEffect(() =>{
+    firestore.collection("tests").get().then(querySnapshot => {
+      console.log(querySnapshot.docs.map(doc => doc.data()))
+    })
+  })
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/shared/firestore.tsx
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app"
+import "firebase/compat/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "time-ledger.firebaseapp.com",
+  projectId: "time-ledger",
+  storageBucket: "time-ledger.appspot.com",
+  messagingSenderId: "785439513253",
+  appId: "1:785439513253:web:1ce1c758335568a3d50b4c",
+  measurementId: "G-VG8XPZ9XLM"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(app);
+
+export default db;
