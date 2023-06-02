3d543e09f3ed4f3f0e6604508f9d45dc25377cc9
hsujc <hsujc513@gmail.com>
2023-06-02 22:42:10 +0800

    add create subgoal/task button

--- src/components/PanelView/GoalView.tsx
+++ src/components/PanelView/GoalView.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { dateFormatStr } from "../../shared/utils";
+import { IoAddCircleOutline } from "react-icons/io5";
+
 import SubGoalView from "./SubGoalView";
 import TaskView from "./TaskView";
 import Goal from "../../shared/goal";
@@ -7,29 +9,58 @@ import NarrowRight from "../../assets/images/narrow-right.png";
 import NarrowDown from "../../assets/images/narrow-down.png";
 import "./GoalView.css";
 
+interface VisibilityState {
+  visibility: "visible" | "hidden";
+}
+
 const GoalView = (goal: Goal) => {
   const [toggle, setToggle] = useState(false);
+  const [visible, setVisible] = useState<VisibilityState>({
+    visibility: "hidden",
+  });
+
+  const addNewGoal = () => {
+    console.log("add new Goal");
+  };
+
   return (
     <div className="panel-goal-view">
-      <div className="panel-goal" onClick={() => setToggle(!toggle)}>
-        <div className="panel-goal-narrow">
-          {!toggle ? (
-            <img
-              className="panel-goal-narrow-right"
-              src={NarrowRight}
-              alt="narrow"
-            />
-          ) : (
-            <img
-              className="panel-goal-narrow-down"
-              src={NarrowDown}
-              alt="narrow"
-            />
-          )}
+      <div
+        className="panel-goal-toggle"
+        onMouseEnter={() => {
+          setVisible({ visibility: "visible" });
+        }}
+        onMouseLeave={() => {
+          setVisible({ visibility: "hidden" });
+        }}
+      >
+        <div className="panel-goal" onClick={() => setToggle(!toggle)}>
+          <div className="panel-goal-narrow">
+            {!toggle ? (
+              <img
+                className="panel-goal-narrow-right"
+                src={NarrowRight}
+                alt="narrow"
+              />
+            ) : (
+              <img
+                className="panel-goal-narrow-down"
+                src={NarrowDown}
+                alt="narrow"
+              />
+            )}
+          </div>
+          <div className="panel-goal-title">{goal.name}</div>
+          <div className="panel-goal-date">
+            {goal.deadline.format(dateFormatStr)}
+          </div>
         </div>
-        <div className="panel-goal-title">{goal.name}</div>
-        <div className="panel-goal-date">
-          {goal.deadline.format(dateFormatStr)}
+        <div
+          className="panel-goal-add"
+          style={visible}
+          onClick={() => addNewGoal()}
+        >
+          <IoAddCircleOutline className="add-icon" />
         </div>
       </div>
       <div className="panel-goal-child">
