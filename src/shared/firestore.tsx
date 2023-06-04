984af4615c986ea7a08485b977f0da9e8a292756
shaoyuchu <r11922047@ntu.edu.tw>
2023-06-04 13:11:17 +0800

    fix bug in fetchScheduleOfDate

--- src/shared/firestore.tsx
+++ src/shared/firestore.tsx
@@ -71,76 +71,84 @@ const fetchScheduleOfDate = async (
     .where("date", "<=", endOfDate.toDate())
     .get();
 
-  const schedules: Schedule[] = [];
+  const promises: Promise<Schedule>[] = [];
   querySnapshot.forEach(async (doc) => {
-    const schdData = doc.data() as ScheduleData;
+    const promise = new Promise<Schedule>(async (resolve) => {
+      const schdData = doc.data() as ScheduleData;
 
-    // retreive taskData => subgoalData => goalData
-    let taskData: TaskData, sgData: SubgoalData | undefined, goalData: GoalData;
-    let taskId: string, sgId: string | undefined, goalId: string;
-    const taskDocRef = await schdData.task.get();
-    taskData = taskDocRef.data() as TaskData;
-    taskId = taskDocRef.id;
-    const taskParentDocRef = await taskData.parent.get();
-    const taskParentId = taskParentDocRef.id;
-    const taskParent = taskParentDocRef.data() as {
-      type: string;
-      [key: string]: any;
-    };
-    if (taskParent.type === Subgoal.type) {
-      // subgoal exists
-      sgData = taskParent as SubgoalData;
-      sgId = taskParentId;
-      const goalDocRef = await sgData.goal.get();
-      goalData = goalDocRef.data() as GoalData;
-      goalId = goalDocRef.id;
-    } else {
-      // subgoal does not exist
-      sgData = undefined;
-      sgId = undefined;
-      goalData = taskParent as GoalData;
-      goalId = taskParentId;
-    }
+      // retreive taskData => subgoalData => goalData
+      let taskData: TaskData,
+        sgData: SubgoalData | undefined,
+        goalData: GoalData;
+      let taskId: string, sgId: string | undefined, goalId: string;
+      const taskDocRef = await schdData.task.get();
+      taskData = taskDocRef.data() as TaskData;
+      taskId = taskDocRef.id;
+      const taskParentDocRef = await taskData.parent.get();
+      const taskParentId = taskParentDocRef.id;
+      const taskParent = taskParentDocRef.data() as {
+        type: string;
+        [key: string]: any;
+      };
+      if (taskParent.type === Subgoal.type) {
+        // subgoal exists
+        sgData = taskParent as SubgoalData;
+        sgId = taskParentId;
+        const goalDocRef = await sgData.goal.get();
+        goalData = goalDocRef.data() as GoalData;
+        goalId = goalDocRef.id;
+      } else {
+        // subgoal does not exist
+        sgData = undefined;
+        sgId = undefined;
+        goalData = taskParent as GoalData;
+        goalId = taskParentId;
+      }
 
-    // retrieve goal => subgoal => task
-    // create dummy objects
-    let goal = await Goal.createFromGoalData(goalId, goalData, false);
-    let sg: Subgoal | undefined, task: Task;
-    if (sgData) {
-      sg = await Subgoal.createFromSubgoalData(sgId!, goal, sgData, false);
-      task = await Task.createFromTaskData(taskId, sg, taskData, false);
-    } else {
-      sg = undefined;
-      task = await Task.createFromTaskData(taskId, goal, taskData, false);
-    }
-
-    // if the schedule belongs to an ongoing goal, point goal/subgoal/task to that in the control context
-    if (goalId in ongoingGoals) {
-      goal = ongoingGoals[goalId];
-      if (sgId !== undefined) {
-        sg = goal.subgoals[sgId];
-        task = sg.tasks[taskId];
+      // retrieve goal => subgoal => task
+      // create dummy objects
+      let goal = await Goal.createFromGoalData(goalId, goalData, false);
+      let sg: Subgoal | undefined, task: Task;
+      if (sgData) {
+        sg = await Subgoal.createFromSubgoalData(sgId!, goal, sgData, false);
+        task = await Task.createFromTaskData(taskId, sg, taskData, false);
       } else {
         sg = undefined;
-        task = goal.tasks[taskId];
+        task = await Task.createFromTaskData(taskId, goal, taskData, false);
       }
-    }
 
-    let startTime: Moment | undefined = undefined;
-    let endTime: Moment | undefined = undefined;
-    if (schdData.startTime) startTime = moment(schdData.startTime.toMillis());
-    if (schdData.endTime) endTime = moment(schdData.endTime.toMillis());
-    const newSchd = new Schedule(
-      goal,
-      sg,
-      task,
-      moment(schdData.date.toDate()),
-      startTime,
-      endTime,
-      doc.id
-    );
-    schedules.push(newSchd);
+      // if the schedule belongs to an ongoing goal, point goal/subgoal/task to that in the control context
+      if (goalId in ongoingGoals) {
+        goal = ongoingGoals[goalId];
+        if (sgId !== undefined) {
+          sg = goal.subgoals[sgId];
+          task = sg.tasks[taskId];
+        } else {
+          sg = undefined;
+          task = goal.tasks[taskId];
+        }
+      }
+
+      let startTime: Moment | undefined = undefined;
+      let endTime: Moment | undefined = undefined;
+      if (schdData.startTime) startTime = moment(schdData.startTime.toMillis());
+      if (schdData.endTime) endTime = moment(schdData.endTime.toMillis());
+      const newSchd = new Schedule(
+        goal,
+        sg,
+        task,
+        moment(schdData.date.toDate()),
+        startTime,
+        endTime,
+        doc.id
+      );
+
+      resolve(newSchd);
+    });
+    promises.push(promise);
   });
+  const schedules = await Promise.all(promises);
+  console.log(schedules);
   return schedules;
 };
 
